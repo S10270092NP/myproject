a5a25c77dfa50457e85f70b90e5b815ed888a90a
=Kyston Teo <=s10270092@connect.np.edu.sg>
2026-05-04 10:14:28 +0800

    Updated index.php to trigger my hairline

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // Sample PHP file
 $name = "Mr Kyston -CNAS";
-$message = "Hello, $name! CNAS has been changed";
+$message = "Hello, $name! CNAS has been changed i want to pluck my hair";
 echo $message;
 ?>
